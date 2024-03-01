060bf316e933f7f3449007fd155f38d1efae41c8
Priyanshu Gour <priyanshugour1@gmail.com>
2024-03-01 13:13:13 +0530

    add chat message service

--- src/main/java/com/priyanshu/websocket/chat/ChatMessageRepository.java
+++ src/main/java/com/priyanshu/websocket/chat/ChatMessageRepository.java
@@ -2,6 +2,9 @@ package com.priyanshu.websocket.chat;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface ChatMessageRepository extends MongoRepository<ChatMessage, String > {
 
+    List<ChatMessage> findByChatId(String s);
 }
--- src/main/java/com/priyanshu/websocket/chat/ChatMessageService.java
+++ src/main/java/com/priyanshu/websocket/chat/ChatMessageService.java
@@ -4,12 +4,37 @@ import com.priyanshu.websocket.chatroom.ChatRoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ChatMessageService {
 
     private final ChatMessageRepository repository;
     private final ChatRoomService chatRoomService;
+
+    public ChatMessage save(ChatMessage chatMessage) {
+        var chatId = chatRoomService.getChatRoomId(
+                chatMessage.getSenderId(),
+                chatMessage.getRecipientId(),
+                true
+        ).orElseThrow( () -> new RuntimeException("Chat room not found"));
+        chatMessage.setChatId(chatId);
+        repository.save(chatMessage);
+        return chatMessage;
+    }
+
+    public List<ChatMessage> findChatMessage(
+            String senderId, String recipientId
+    ){
+        var chatId = chatRoomService.getChatRoomId(
+                senderId,
+                recipientId,
+                false
+        );
+        return chatId.map(repository::findByChatId).orElse(new ArrayList<>());
+    }
 
 
 }
